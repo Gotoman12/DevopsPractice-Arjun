74f5f60db63e54398234f077df85ecbf533c7421
Mslliksrjun <mallikarjunckm@example.com>
2025-11-19 12:05:23 +0530

    added

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 print("My first python script")
 
 
+import sys
+
+# Get numbers from command line arguments
+num1 = float(sys.argv[1])
+num2 = float(sys.argv[2])
+
+# Compute the sum
+result = num1 + num2
+
+# Output the result
+print(result)
