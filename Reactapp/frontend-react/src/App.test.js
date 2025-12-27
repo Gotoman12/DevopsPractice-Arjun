81d3ed9a83b31a4f57fcef107de846ca397b74ed
Mslliksrjun <mallikarjunckm@example.com>
2025-12-27 16:38:05 +0530

    deleted

--- Reactapp/frontend-react/src/App.test.js
+++
@@ -1,9 +0,0 @@
-
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders clothing store heading', () => {
-  render(<App />);
-  const heading = screen.getByText(/Clothing Store/i);
-  expect(heading).toBeInTheDocument();
-});
